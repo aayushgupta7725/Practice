f7393fc8b5bea7337cd74325836d59e8ec82cd46
aayushgupta7725 <aayushgupta7725@gmail.com>
2026-06-15 19:31:55 +0530

    new commit

--- file.c
+++ file.c
@@ -1,41 +1,30 @@
-#include<stdio.h>
-void merge_sort(int arr[], int low, int high)
-{   int mid;
-    if(low<high)
-    {
-        mid=(low+high)/2;
-        merge_sort(arr,low,mid);
-        merge_sort(arr,mid+1,high);
-        merge(arr,low,mid,high);
-    }
-}
-void merge(int arr[], int low, int mid, int high)
-{
-    int n1=mid-low+1, n2=high-mid,i,j,k;
-    int left[n1], right[n2];
-    for(i=0;i<n1;i++)
-    {left[i]=arr[low+i];}
-    for(j=0;j<n2;j++)
-    {right[j]=arr[mid+1+j];}
-    i=0;
-    j=0;
-    k=low;
-    while((i<n1)&&(j<n2))
-    {
-        if(left[i]<=right[j])
-        {arr[k++]=left[i++];}
-        else
-        {arr[k++]=right[j++];}
-    }
-    while(i<n1)
-    arr[k++]=left[i++];
-    while(j<n2)
-    arr[k++]=right[j++];
-}
+#include <stdio.h>
+#include <stdlib.h>
 void main()
-{   int n=5,i;
-    int arr[5]={3,5,62,43,1};
-    merge_sort(arr,0,5);
+{
+    int requests[]={98,183,37,122,14,124,65,67};
+    int n=8, req, current=53, tracks=200, i, j, temp, total=0;
+    for(i=1;i<n;i++)
+    {
+        j=i-1;
+        temp=requests[i];
+        while((j>=0)&&(requests[j]>temp))
+        {
+            requests[j+1]=requests[j];
+            j=j-1;
+        }
+        requests[j+1]=temp;
+    }
+    total=total+abs(current-requests[0])+abs(requests[0]-requests[n-1]);
     for(i=0;i<n;i++)
-    {printf("%d ", arr[i]);}
+    {
+        if(requests[i]>current)
+        {
+            req=i;
+            break;
+        }
+    }
+    total=total+abs(requests[n-1]-requests[req]);
+    printf("%d",total);
+
 }
